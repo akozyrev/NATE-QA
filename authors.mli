48374a88a3a58ea8594aae889a5b50c0bf0fbd03
Andrei <akozyrevjr@gmail.com>
2019-04-09 22:25:49 -0400

    authors files

---
+++ authors.mli
@@ -0,0 +1,9 @@
+(**
+   Metadata about this submission.
+   @author Andrei Kozyrev aak85, Shreeya Gad sg988, Junan Qu jq77,
+   Jane Zhang jz393.
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
