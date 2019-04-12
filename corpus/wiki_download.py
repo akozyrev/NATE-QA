9e7d1b8c4101cfb63bf387bb2ef8eeb022c6a024
Andrei <akozyrevjr@gmail.com>
2019-04-12 17:13:50 -0400

    more corpus files

--- corpus/wiki_download.py
+++ corpus/wiki_download.py
@@ -1,6 +1,13 @@
 import wikipedia
 
-topics = ["Mathematics"]
+topics = ["Mathematics", "David Gries", "Artificial Intelligence",
+"Computer Vision", "Natural Language Processing", "Deep Learning", "Mark Zuckerberg",
+"YouTube", "Apple", "IPhone", "Silicon Valley", "Spotify", "Pinterest",
+"Slack Technologies", "Scott Belsky", "Uber", "Warby Parker", "Cornell University",
+"Stanford University", "Turing Award", "Intel", "Deepmind", "Reinforcement Learning",
+"Steve Jobs", "Venture Capital", "IPad", "Algorithm", "Privacy",
+"Jeff Bezos", "Bill Gates", "Elon Musk", "Tesla", "SpaceX", "Tracy Chou",
+"Sheryl Sandberg", "Grace Hopper", "Anita Borg", "Barbara Liskov"]
 for topic in topics:
   stuff = wikipedia.page(topic).content
 
