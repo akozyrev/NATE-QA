08133dadaccc08e57b0aa69f24df5825ca9d67e5
jz393 <jane.zhangjiaye@gmail.com>
2019-04-12 23:18:14 -0400

    get started on tokenize

--- tokenizer.ml
+++ tokenizer.ml
@@ -1,12 +1,31 @@
 module Tokenizer = struct
 
-  let sent_tokenize =
+type t = {
+  sentences : string list;
+  words : string list;
+}
 
+(*
+1. seperate by period
+2. remove all \n if applicable
+*)
+  let sent_tokenize (text:string) : string list =
     failwith "Unimplemented"
 
-  let word_tokenize = 
+
+(*TODO: remove filler words, i.e. a, the, is ???
+1. make all lowercase
+2. remove all punctuation except - ;
+*)
+  let word_tokenize (text:string) : string list = 
     failwith "Unimplemented"
 
+
+  let tokenize (text:string) : t =
+    {
+      sentences = sent_tokenize text;
+      words = word_tokenize text;
+    }
 
 
 end
