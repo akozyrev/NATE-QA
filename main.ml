0fc68ea961a757bc726d6d011cdcb51dc01d069d
Shreeya Gad <shreeyagad@Shreeyas-MacBook-Air.local>
2019-04-23 23:21:56 -0400

    added diff algo

--- main.ml
+++ main.ml
@@ -35,15 +35,15 @@ That being said, I am still getting smarter and faster with each day!\n"
 
 (** [process input] will return the correct response to the input
     the user provides. *)
-let process input = 
-  let qeustion_vec =
+let process_cos input = 
+  let question_vec =
     Extract.vectorize 
       (Tokenizer.word_tokenize input)
       Extract.count_all_unique_words 
       (Extract.word2vec_dict Extract.count_all_unique_words)
   in
   let topic = Extract.add_tfidf input in 
-  let cos_response = Extract.find_max_cosine topic qeustion_vec [""] 0.0 in
+  let cos_response = Extract.find_max_cosine topic question_vec [""] 0.0 in
   let a_response = Autocorrect.check_correctness input in
   let sug = Suggestion.suggestion input in
   match a_response with 
@@ -55,22 +55,35 @@ let process input =
       | _, _  ->  (String.concat " " cos_response) ^ "\n" ^ sug ^ "\n" end
   | _ -> "Autocorrect found word(s) not identified: " ^ a_response 
 
+let process_jac input = 
+  let response = Extract.get_response input in
+  let a_response = Autocorrect.check_correctness input in
+  let sug = Suggestion.suggestion input in
+
+  match a_response with 
+  | "all correct" -> begin match input, response with 
+      | "about", _ -> about
+      | "examples", _ -> examples 
+      | "help", _ -> help 
+      | _ , "" -> "Please input a valid question.\n"
+      | _, _  ->  response ^ "\n" ^ sug ^ "\n" end
+  | _ -> "Autocorrect found word(s) not identified: " ^ a_response 
 
 (** [response input] provides the user with a response to the input
     they provide. It also prompts the user for another question, if
     the user wishes to continue speaking to the chatbot. *)
-let rec response input =
+let rec response algo input =
   match input with
   | "bye" -> ANSITerminal.(print_string [magenta; Bold] (("\nThank you for talking to me!\n")^("")))
   | input ->  
-    let output = process input in
+    let output = algo input in
     print_string  "\n";
     if output = "Please input a valid question.\n" then 
       ANSITerminal.(print_string [red; Bold] output) else
       ANSITerminal.(print_string [blue; Bold] output);
     print_string  "\n> ";
 
-    let new_input = Pervasives.read_line () in response new_input
+    let new_input = Pervasives.read_line () in response algo new_input
 
 (** [main ()] greets the user and starts the chatbot. *)
 let main () =
@@ -79,8 +92,32 @@ let main () =
   (* print_endline (examples); *)
   (* print_endline (help); *)
   ANSITerminal.(print_string [blue; Bold] help);
+  print_string  "\n";
+  ANSITerminal.(print_string [green; Bold] "Which algorithm would you like to use for this session?\n");
+  ANSITerminal.(print_string [green; Bold] "\n> 1 for Jaccard\n> 2 for Cosine Similarity\n");
+  print_string "\n> ";
+  let algo = Pervasives.read_line () in
+  begin
+  match algo with
+  | "1" -> 
+  begin 
+  ANSITerminal.(print_string [green; Bold] "\nJaccard will be used for this session.\n");
   print_string  "\n> ";
-  response (Pervasives.read_line ())
+  response process_jac (Pervasives.read_line ());
+  end
+  | "2" ->
+  begin 
+  ANSITerminal.(print_string [green; Bold] "\nCosine Similarity will be used for this session.\n");
+  print_string  "\n> ";
+  response process_cos (Pervasives.read_line ());
+  end
+  | _ -> 
+  begin 
+  ANSITerminal.(print_string [green; Bold] "\nInvalid input; Jaccard will be used by default.\n");
+  print_string  "\n> ";
+  response process_jac (Pervasives.read_line ());
+  end
+  end
 
 (* Executes the chatbot. *)
 
